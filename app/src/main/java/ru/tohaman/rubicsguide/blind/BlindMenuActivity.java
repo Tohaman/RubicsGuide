a81532d821dff7b2d7fdfd83563070690abcf020
Tohaman <avrozovu@yandex.ru>
2017-06-21 16:14:55 +0300

    Commit Home

---
+++ app/src/main/java/ru/tohaman/rubicsguide/blind/BlindMenuActivity.java
@@ -0,0 +1,15 @@
+package ru.tohaman.rubicsguide.blind;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ru.tohaman.rubicsguide.R;
+
+public class BlindMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blind_menu);
+    }
+}
---
+++ app/src/main/java/ru/tohaman/rubicsguide/blind/ScambleFragment.java
@@ -0,0 +1,10 @@
+package ru.tohaman.rubicsguide.blind;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Toha on 21.06.2017.
+ */
+
+public class ScambleFragment extends Fragment {
+}
