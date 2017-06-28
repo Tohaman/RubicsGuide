a70a2b73e63757783d8c1935e702d10ad9a54c2f
Tohaman <avrozovu@yandex.ru>
2017-06-28 21:31:17 +0300

    Commit Home (сделано редактирование скрамбла/коментария)

--- app/src/main/java/ru/tohaman/rubicsguide/blind/AzbukaFragment.java
+++ app/src/main/java/ru/tohaman/rubicsguide/blind/AzbukaFragment.java
@@ -8,6 +8,8 @@ import android.support.v4.content.ContextCompat;
 import android.text.Html;
 import android.text.Spanned;
 import android.text.method.LinkMovementMethod;
+import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
