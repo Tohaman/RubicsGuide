8449f5b02377490e9b068a126811d13cfdbae310
Tohaman <avrozovu@yandex.ru>
2017-07-20 20:19:37 +0300

    Commit Note (Редактируем таймер)

--- app/src/main/java/ru/tohaman/rubicsguide/timer/TimerFragment.java
+++ app/src/main/java/ru/tohaman/rubicsguide/timer/TimerFragment.java
@@ -1,9 +1,14 @@
 package ru.tohaman.rubicsguide.timer;
 
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -11,7 +16,11 @@ import android.widget.Chronometer;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import ru.tohaman.rubicsguide.CommentFragment;
 import ru.tohaman.rubicsguide.R;
+import ru.tohaman.rubicsguide.listpager.PagerFragment;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * Created by anton on 19.07.17.
@@ -20,6 +29,16 @@ import ru.tohaman.rubicsguide.R;
 public class TimerFragment extends Fragment {
     TextView timerTextView;
     long startTime = 0;
+    boolean leftHandDown = false;
+    boolean rightHandDown = false;
+    boolean timerReady = false;
+    boolean timerStart = false;
+    View mLeftLight;
+    View mRightLight;
+
+    //TODO Сделать новую картинку для таймера
+
+    //TODO Сделать, чтобы на данной активности экран не гас.
 
     //runs without a timer by reposting this handler at the end of the runnable
     Handler timerHandler = new Handler();
@@ -28,7 +47,7 @@ public class TimerFragment extends Fragment {
         @Override
         public void run() {
             long millis = System.currentTimeMillis() - startTime;
-            int sotki = (int) (millis%1000)/10;
+            int sotki = (int) (millis%1000)/10;             // сотые доли секунды
             int seconds = (int) (millis / 1000);
             int minutes = seconds / 60;
             seconds = seconds % 60;
@@ -42,42 +61,41 @@ public class TimerFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_timer, container, false);
         timerTextView = (TextView) view.findViewById(R.id.texttime);
+        timerTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View v) {
+                timerTextView.setText(R.string.init_start_time);
+            }
+        });
 
         LinearLayout mRightHand = (LinearLayout) view.findViewById(R.id.rigth_hand);
         LinearLayout mLeftHand = (LinearLayout) view.findViewById(R.id.left_hand);
+        mLeftLight = (View) view.findViewById(R.id.left_light);
+        mRightLight = (View) view.findViewById(R.id.right_light);
 
-        mRightHand.setOnClickListener(new View.OnClickListener() {
+        // Обработка прикосновения к левому лэйауту
+        mLeftHand.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public void onClick (View v) {
-                timerHandler.removeCallbacks(timerRunnable);
+            public boolean onTouch (View v, MotionEvent event) {
+                int action = event.getActionMasked();
+                leftHandDown = OnTouchAction(leftHandDown,action,mLeftLight);
+                timerReady = leftHandDown & rightHandDown;
+                return true;
             }
         });
 
-        mLeftHand.setOnClickListener(new View.OnClickListener() {
+        // Обработка прикосновения к правому лэйауту
+        mRightHand.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public void onClick (View v) {
-                startTime = System.currentTimeMillis();
-                timerHandler.postDelayed(timerRunnable, 0);
+            public boolean onTouch (View v, MotionEvent event) {
+                int action = event.getActionMasked();
+         //       Log.d(TAG, String.valueOf(action));
+                rightHandDown = OnTouchAction(rightHandDown,action,mRightLight);
+                timerReady = leftHandDown & rightHandDown;
+                return true;
             }
         });
 
-//        StartStopButton = (Button) view.findViewById(R.id.button_start);
-//        StartStopButton.setText("start");
-//        StartStopButton.setOnClickListener(new View.OnClickListener() {
-//
-//            @Override
-//            public void onClick(View v) {
-//                Button b = (Button) v;
-//                if (b.getText().equals("stop")) {
-//                    timerHandler.removeCallbacks(timerRunnable);
-//                    b.setText("start");
-//                } else {
-//                    startTime = System.currentTimeMillis();
-//                    timerHandler.postDelayed(timerRunnable, 0);
-//                    b.setText("stop");
-//                }
-//            }
-//        });
         return view;
     }
 
@@ -85,8 +103,48 @@ public class TimerFragment extends Fragment {
     public void onPause() {
         super.onPause();
         timerHandler.removeCallbacks(timerRunnable);
-//        StartStopButton.setText("start");
     }
 
+    private boolean OnTouchAction (boolean hand, int action, View handLight) {
+        if (action == MotionEvent.ACTION_DOWN) {  //
+            hand = true;
+            handLight.setBackground(getDrawable(R.drawable.green_oval));
+            if (leftHandDown & rightHandDown) {     //если обе руки положили, то останавливаем таймер, если нет, ждем вторую руку
+                timerStart = false;
+                StopTimer();
+            }
+        }
+        if (action == MotionEvent.ACTION_UP) {    // Если убрали все пальцы с лэйаута (отпустили)
+            hand = false;
+            handLight.setBackground(getDrawable(R.drawable.red_oval));
+            if (timerReady) {
+                StartTimer();
+                timerStart = true;
+            }
+        }
+        return hand;
+    }
+
+
+    private void StartTimer() {
+        startTime = System.currentTimeMillis();
+        timerHandler.postDelayed(timerRunnable, 0);
+    }
+
+    private void StopTimer() {
+        timerHandler.removeCallbacks(timerRunnable);
+    }
+
+
+    @SuppressWarnings("deprecation")
+    private Drawable getDrawable(int ResID) {
+        Drawable drawable = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            drawable = getResources().getDrawable(ResID, null);
+        }else {
+            drawable = getResources().getDrawable(ResID);
+        }
+        return drawable;
+    }
 
 }
