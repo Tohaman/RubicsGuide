7ddd4aa85f4fd04961b4c6fa760d94f72c684d09
Tohaman <avrozovu@yandex.ru>
2017-07-20 23:21:11 +0300

    Commit HomeWindows  (Осталось не гасить экран таймера)

--- app/src/main/java/ru/tohaman/rubicsguide/timer/TimerFragment.java
+++ app/src/main/java/ru/tohaman/rubicsguide/timer/TimerFragment.java
@@ -7,6 +7,7 @@ import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.util.Log;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
@@ -35,8 +36,8 @@ public class TimerFragment extends Fragment {
     boolean timerStart = false;
     View mLeftLight;
     View mRightLight;
-
-    //TODO Сделать новую картинку для таймера
+    protected static final String TAG = "GestureDetectorMain";
+    private GestureDetector mGestureDetector;
 
     //TODO Сделать, чтобы на данной активности экран не гас.
 
@@ -47,7 +48,7 @@ public class TimerFragment extends Fragment {
         @Override
         public void run() {
             long millis = System.currentTimeMillis() - startTime;
-            int sotki = (int) (millis%1000)/10;             // сотые доли секунды
+            int sotki = (int) (millis % 1000) / 10;             // сотые доли секунды
             int seconds = (int) (millis / 1000);
             int minutes = seconds / 60;
             seconds = seconds % 60;
@@ -61,10 +62,13 @@ public class TimerFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_timer, container, false);
         timerTextView = (TextView) view.findViewById(R.id.texttime);
-        timerTextView.setOnClickListener(new View.OnClickListener() {
+
+        mGestureDetector = new GestureDetector(getActivity(), new GestureDetector.SimpleOnGestureListener() {
             @Override
-            public void onClick (View v) {
+            public boolean onDoubleTap(MotionEvent e) {
+                //Log.i(TAG, "onDoubleTap");
                 timerTextView.setText(R.string.init_start_time);
+                return true;
             }
         });
 
@@ -76,10 +80,12 @@ public class TimerFragment extends Fragment {
         // Обработка прикосновения к левому лэйауту
         mLeftHand.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public boolean onTouch (View v, MotionEvent event) {
+            public boolean onTouch(View v, MotionEvent event) {
+                mGestureDetector.onTouchEvent(event);
                 int action = event.getActionMasked();
-                leftHandDown = OnTouchAction(leftHandDown,action,mLeftLight);
-                timerReady = leftHandDown & rightHandDown;
+//                Log.d(TAG, String.valueOf(action));
+                leftHandDown = OnTouchAction(leftHandDown, rightHandDown, action, mLeftLight);
+//                Log.d(TAG, String.valueOf(leftHandDown));
                 return true;
             }
         });
@@ -87,15 +93,15 @@ public class TimerFragment extends Fragment {
         // Обработка прикосновения к правому лэйауту
         mRightHand.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public boolean onTouch (View v, MotionEvent event) {
+            public boolean onTouch(View v, MotionEvent event) {
+                mGestureDetector.onTouchEvent(event);
                 int action = event.getActionMasked();
-         //       Log.d(TAG, String.valueOf(action));
-                rightHandDown = OnTouchAction(rightHandDown,action,mRightLight);
-                timerReady = leftHandDown & rightHandDown;
+//                Log.d(TAG, String.valueOf(action));
+                rightHandDown = OnTouchAction(rightHandDown, leftHandDown, action, mRightLight);
+//                Log.d(TAG, String.valueOf(rightHandDown));
                 return true;
             }
         });
-
         return view;
     }
 
@@ -105,24 +111,30 @@ public class TimerFragment extends Fragment {
         timerHandler.removeCallbacks(timerRunnable);
     }
 
-    private boolean OnTouchAction (boolean hand, int action, View handLight) {
-        if (action == MotionEvent.ACTION_DOWN) {  //
-            hand = true;
+
+    private boolean OnTouchAction(boolean action_hand, boolean second_hand, int action, View handLight) {
+        if (action == MotionEvent.ACTION_DOWN){  //если что-то нажато (только первое нажатие)
+            action_hand = true;
             handLight.setBackground(getDrawable(R.drawable.green_oval));
-            if (leftHandDown & rightHandDown) {     //если обе руки положили, то останавливаем таймер, если нет, ждем вторую руку
-                timerStart = false;
-                StopTimer();
+            if (second_hand){     //если обе руки положили (action_hand = true), то что-то делаем, если нет, ждем вторую руку
+                if (!timerStart){              // если таймер не запущен, ставим признак, что таймер готов к запуску
+                    timerReady = true;
+                } else {                        // если запущен, то останавливаем таймер
+                    StopTimer();
+                    timerStart = false;
+                }
             }
         }
-        if (action == MotionEvent.ACTION_UP) {    // Если убрали все пальцы с лэйаута (отпустили)
-            hand = false;
-            handLight.setBackground(getDrawable(R.drawable.red_oval));
-            if (timerReady) {
+        if (action == MotionEvent.ACTION_UP){    // Если убрали все пальцы с лэйаута (отпустили)
+            action_hand = false;                    // поставили, что текущая рука убрана
+            handLight.setBackground(getDrawable(R.drawable.red_oval));  //окрасили кружок красным
+            if (timerReady){                           // если была готовность таймер, то его запустили
                 StartTimer();
-                timerStart = true;
+                timerStart = true;                      // поставили признак, что таймер запущен
+                timerReady = false;                     // сняли признак, что готов к старту
             }
         }
-        return hand;
+        return action_hand;                             // вернули текущее значение активной руки
     }
 
 
@@ -141,7 +153,7 @@ public class TimerFragment extends Fragment {
         Drawable drawable = null;
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
             drawable = getResources().getDrawable(ResID, null);
-        }else {
+        } else {
             drawable = getResources().getDrawable(ResID);
         }
         return drawable;
