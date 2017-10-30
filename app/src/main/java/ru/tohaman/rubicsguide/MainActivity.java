278c94c8b011ca889cb6a3a78f5560ac55fca667
Tohaman <avrozovu@yandex.ru>
2017-10-30 17:05:58 +0300

    Commit HomeWindows Версия для PlayMarket 20171030

--- app/src/main/java/ru/tohaman/rubicsguide/MainActivity.java
+++ app/src/main/java/ru/tohaman/rubicsguide/MainActivity.java
@@ -1,13 +1,10 @@
 package ru.tohaman.rubicsguide;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatDelegate;
 import android.view.MenuItem;
 import android.widget.Toast;
 
