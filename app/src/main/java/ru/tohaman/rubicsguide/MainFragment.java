11ca476d0f2a824960b979bbea939b477d1d1b2a
Tohaman <avrozovu@yandex,ru>
2017-12-13 17:32:41 +0300

    Work comp: Commit. Ключик вынесен в отдельный файл.

--- app/src/main/java/ru/tohaman/rubicsguide/MainFragment.java
+++ app/src/main/java/ru/tohaman/rubicsguide/MainFragment.java
@@ -55,6 +55,7 @@ public class MainFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        //return inflater.inflate(R.layout.fragment_about,null);
         //Инициализируем синглет ListPagerLab, в котором будет база.
         ListPagerLab listPagerLab = ListPagerLab.get(getActivity());
 
